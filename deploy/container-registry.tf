2f66c189069e284f639550bde2b1a9764b95525a
bevzd <dbevzpr@mail.ru>
2024-12-22 20:20:39 +0200

    refactoring

--- deploy/container-registry.tf
+++ deploy/container-registry.tf
@@ -53,7 +53,7 @@ resource "yandex_serverless_container" "authorizer" {
   memory = 128
   folder_id = local.folder_id
   image {
-    url = "cr.yandex/${yandex_container_registry.sls.id}/auth:latest"
+    url = "cr.yandex/crp31fj9u7aj7h70jn5t/auth:11031c54e9db0c977c658488c44e4fb69818af39"
   }
   depends_on = [null_resource.build_and_push_authorizer_image]
 }
