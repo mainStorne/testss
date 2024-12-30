87f2a22926265a6a09632791af23ccdea5778e3a
bevzd <dbevzpr@mail.ru>
2024-12-30 13:25:52 +0200

    added json logs

--- services/auth/main.py
+++ services/auth/main.py
@@ -1,5 +1,10 @@
 import uvicorn
 import os
+from pathlib import Path
+
+log_config = Path(__file__).parent / 'log_config.yml'
 
 if __name__ == '__main__':
-    uvicorn.run('application.app:app', host='0.0.0.0', port=int(os.getenv('PORT', '8080')), reload=True)
+    uvicorn.run('application.app:app', host='0.0.0.0', port=int(os.getenv('PORT', '8080')), reload=True,
+                log_config=str(log_config.absolute())
+                )
